2b840a6c5bf407e23e6f750a56015f4ec01bf84a
Nicolò Toscani <toscani.nicolo90@gmail.com>
2022-12-08 22:50:36 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
 // Custom Web Control code
+
+// Change measures tab
+function openMeasures(measuresTab) {
+    var i;
+    var x = document.getElementsByClassName("measures");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(measuresTab).style.display = "block";  
+  }
